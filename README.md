2c22dda40b9733f9895ebf839620392d6e7a854b
ian <yihengz@outlook.com>
2024-02-03 18:38:43 +0800

    add biome

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ TODO:
 - [ ] change to bun env since hono on node is slower than express https://github.com/SaltyAom/bun-http-framework-benchmark
 - [ ] wait aws lambda bun official support https://medium.com/@mitchellkossoris/serverless-bun-vs-node-benchmarking-on-aws-lambda-ecd4fe7c2fc2
 - [ ] wait serverless framework bun official support
+- [ ] remove @types/node ts-node pnpm-lock.yaml
--- serverless.ts
+++ serverless.ts
@@ -1,27 +1,27 @@
-import type { AWS } from "@serverless/typescript";
+import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
-  app: "app-name",
-  service: "lambda-name",
+  app: 'app-name',
+  service: 'lambda-name',
 
-  frameworkVersion: "3",
-  configValidationMode: "error",
+  frameworkVersion: '3',
+  configValidationMode: 'error',
 
   provider: {
-    name: "aws",
-    runtime: "nodejs20.x",
-    architecture: "arm64",
+    name: 'aws',
+    runtime: 'nodejs20.x',
+    architecture: 'arm64',
     timeout: 29, // the maximum api gateway timeout is 29s
     tags: {
-      developer: "devName",
-      project: "${self:app}",
-      service: "${self:service}",
+      developer: 'devName',
+      project: '${self:app}',
+      service: '${self:service}',
     },
     logRetentionInDays: 60,
     environment: {
-      STAGE: "${sls:stage}",
-      STAGE_PATH_PREFIX: "/${sls:stage}",
-      NODE_OPTIONS: "--enable-source-maps",
+      STAGE: '${sls:stage}',
+      STAGE_PATH_PREFIX: '/${sls:stage}',
+      NODE_OPTIONS: '--enable-source-maps',
     },
   },
 
@@ -31,19 +31,19 @@ const serverlessConfiguration: AWS = {
 
   functions: {
     api: {
-      handler: "src/lambda.handler",
+      handler: 'src/lambda.handler',
       events: [
         {
           http: {
-            method: "ANY",
-            path: "/",
+            method: 'ANY',
+            path: '/',
             cors: true,
           },
         },
         {
           http: {
-            method: "ANY",
-            path: "{proxy+}",
+            method: 'ANY',
+            path: '{proxy+}',
             cors: true, // https://www.serverless.com/framework/docs/providers/aws/events/apigateway#enabling-cors
           },
         },
@@ -52,7 +52,7 @@ const serverlessConfiguration: AWS = {
   },
 
   // https://github.com/floydspace/serverless-esbuild
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
 };
 
 module.exports = serverlessConfiguration;
